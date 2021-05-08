2cebbc96abc371a10cf543a46a12e733e013496d
Ninh Phạm <manhninh91@gmail.com>
2021-05-08 21:42:18 +0700

    update plugin

--- src/socketHandlers/events/TradingApi/index.ts
+++ src/socketHandlers/events/TradingApi/index.ts
@@ -11,7 +11,14 @@ const changeProtectStatus = (_socket: Socket) => (protectStatus: PROTECT_STATUS)
   global.protectBO = protectStatus;
 };
 
+const updateProtectLevel = (_socket: Socket) => ({protectLevel1, protectLevel2, protectLevel3}) => {
+  global.protectLevel1 = protectLevel1;
+  global.protectLevel2 = protectLevel2;
+  global.protectLevel3 = protectLevel3;
+};
+
 export default (socket: Socket) => ({
   addProcessJob: addProcessJob(socket),
   changeProtectStatus: changeProtectStatus(socket),
+  updateProtectLevel: updateProtectLevel(socket),
 });
